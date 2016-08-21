09f3830a1af94c290b3fec1bbb7536533e7c1496
Rohan Koodli <RK900@users.noreply.github.com>
2016-08-21 09:43:51 -0700

    added projections
    low variance, high variance, med variance, constant fertility

--- ipc_microsim_tool_sklearn.py
+++ ipc_microsim_tool_sklearn.py
@@ -1,17 +1,18 @@
+
 '''
 Rohan Koodli
-scikit-learn implementation of WPP dataset
+scikit-learn implementation of Electricity Consumption Surveys project
 UN-DESA Modeling Tools
 '''
 
 import pandas as pd
 import numpy as np
 
-estimates = pd.read_csv('\WPP-ESTIMATES-1950-2015.tab.txt',
+estimates = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\WPP-ESTIMATES-1950-2015.tab.txt',
                         sep='\t', index_col=None, na_values='')
 
 
-projections = pd.read_csv('\WPP-PROJECTIONS-2015-2100.tab.txt',
+projections = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\WPP-PROJECTIONS-2015-2100.tab.txt',
                           sep='\t',index_col=None,na_values='')
 
 #print projections[0:2]
@@ -26,17 +27,20 @@ projections.drop(projections.columns[[0,1,2,3,70,71,72,73,74,75,76,77,78,79,80,8
 medVariance_projections = np.array(projections[0:273])
 highVariance_projections = np.array(projections[274:547])
 lowVariance_projections = np.array(projections[548:821])
+constantFertility_projections = np.array(projections[822:1095])
 
 estimates = np.array(estimates)
 from sklearn import ensemble,tree,neighbors
 rfr = ensemble.RandomForestRegressor()
 dtr = tree.DecisionTreeRegressor()
 knr = neighbors.KNeighborsRegressor()
+#nnn = neural_network.BernoulliRBM()
 
 from sklearn.cross_validation import cross_val_score
 print cross_val_score(rfr,estimates,medVariance_projections)
 print cross_val_score(dtr,estimates,medVariance_projections)
 print cross_val_score(knr,estimates,medVariance_projections)
+
 
 
 
