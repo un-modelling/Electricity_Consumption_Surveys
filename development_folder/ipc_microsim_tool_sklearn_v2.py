535d695c820ad57278ea72dce6003c6ebf0a6985
RK900 <rovik05@gmail.com>
2016-10-11 16:30:31 -0700

    Added high variance projections and scatterplot of data

--- development_folder/ipc_microsim_tool_sklearn_v2.py
+++ development_folder/ipc_microsim_tool_sklearn_v2.py
@@ -1,44 +1,47 @@
-'''
-Author: Rohan Koodli
-Implementing Electricity Consumption Surveys with sklearn
-Version 2.0
-This file is under active development.
-'''
-
 import pandas as pd
 import numpy as np
+import os
 
-# reading in aztlan data on income, kilowatt-hour per capita, and wgt
-aztlan = pd.read_csv('\Users\Rohan\Documents\GitHub\Electricity_Consumption_Surveys\ipc_microsim_tool\data\example_aztlan_seed.tab.txt',
-                     sep='\t', index_col=None, na_values='')
+#getting current working directory
+path = os.getcwd()
 
-X = np.array(aztlan['income'])
-#print (X)
-#X = X[:, None]
-# X is all the income data values
-X1 = []
-for i in X:
-    X1.append([i])
+estimates = pd.read_csv(path + '\data\WPP-ESTIMATES-1950-2015.tab.txt',
+                        sep='\t', index_col=None, na_values='')
 
-#print X1
-y = np.array(aztlan['kwhpc'])
-# y is all the kilowatt-hour values
-print len(y)
 
-from sklearn import neighbors,tree,ensemble,svm
+projections = pd.read_csv(path + '/data/WPP-PROJECTIONS-2015-2100.tab.txt',
+                          sep='\t',index_col=None,na_values='')
+
+#print projections[0:2]
+
+#estimates.drop(estimates.columns[[0,1,2,3]],axis=1,inplace=True)
+
+estimates.drop(estimates.columns[[0,1,2,3]], axis=1, inplace=True)
+#print estimates[0:2]
+projections.drop(projections.columns[[0,1,2,3,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89]], axis=1, inplace=True)
+#print projections[0:2]
+
+
+# putting data in numpy arrays
+medVariance_projections = np.array(projections[0:273])
+highVariance_projections = np.array(projections[274:547])
+lowVariance_projections = np.array(projections[548:821])
+constantFertility_projections = np.array(projections[822:1095])
+
+estimates = np.array(estimates)
+
+from sklearn import ensemble,tree,neighbors
 rfr = ensemble.RandomForestRegressor()
 dtr = tree.DecisionTreeRegressor()
 knr = neighbors.KNeighborsRegressor()
-svr = svm.SVR()
 #nnn = neural_network.BernoulliRBM()
-''''''
-from sklearn.cross_validation import cross_val_score
-print cross_val_score(rfr,X1,y)
-print cross_val_score(dtr,X1,y)
-print cross_val_score(knr,X1,y)
-print cross_val_score(svr,X1,y)
-''''''
-from matplotlib import pyplot as plt
-import seaborn; seaborn.set()
 
-plt.scatter(X1,y)
+from sklearn.cross_validation import cross_val_score
+print 'Random Forests',cross_val_score(rfr,estimates,medVariance_projections)
+print 'Decision Trees',cross_val_score(dtr,estimates,medVariance_projections)
+print 'Nearest Neighbors',cross_val_score(knr,estimates,medVariance_projections)
+
+from sklearn.cross_validation import cross_val_score
+print 'Random Forests',cross_val_score(rfr,estimates,highVariance_projections)
+print 'Decision Trees',cross_val_score(dtr,estimates,highVariance_projections)
+print 'Nearest Neighbors',cross_val_score(knr,estimates,highVariance_projections)
